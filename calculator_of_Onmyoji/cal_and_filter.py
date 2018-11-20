3542e8e2c4342c77c3559a7683f603cc536a7a5f
linshier <linshier@gmail.com>
2018-11-20 22:04:26 +0800

    change function placement

--- calculator_of_Onmyoji/cal_and_filter.py
+++ calculator_of_Onmyoji/cal_and_filter.py
@@ -32,6 +32,7 @@ selected = set()
 effect_min_speed = 0
 effect_max_speed = 200
 damage_min_speed = 11
+damage_max_speed = 500
 damage_min_crit_rate = 90
 attack_buf_base = 100
 crit_damage_base = 160
@@ -195,6 +196,159 @@ def filter_soul(prop_value, prop_value_l2, prop_value_l4, prop_value_l6,
     #print('result: %s' % result)
     #print(('%s %s' % (d, comb_sum)).decode('raw_unicode_escape'))
     return result, comb_sum, suit_buf_max
+
+def make_result(data_dict, res, com):
+    d1, d2, d3, d4, d5, d6 = data_dict.values()
+    comb_data = {'sum': com,
+                 'info': [find_item(d1, res[0]),
+                          find_item(d2, res[1]),
+                          find_item(d3, res[2]),
+                          find_item(d4, res[3]),
+                          find_item(d5, res[4]),
+                          find_item(d6, res[5])]
+                }
+    return comb_data
+
+def map2list(codes, dx):
+    l = []
+    mitama_codes_num = len(codes)
+    #print(('map2list: %s' % dx).decode('raw_unicode_escape'))
+    for i in dx:
+        code = -1
+        k = i.keys()[0]
+        v = i.values()[0]
+	#print(('map2list: %s' % v).decode('raw_unicode_escape'))
+        for j in xrange(mitama_codes_num):
+            if codes[j] == v[suit]:
+                code = j
+                break
+        val = int(0)
+        val += int(v[effect])
+        val <<= 8
+        val += (int(v[attack_buf]) + int(v[attack]*100/attack_hero))
+        val <<= 8
+        val += int(v[crit_rate])
+        val <<= 8
+        val += int(v[crit_damage])
+        val <<= 8
+        val += int(v[speed])
+        l.append([val,
+                  (1 << (3 * code)) if (code >= 0) else 0,
+                  k])
+    return l
+
+def list2map(codes, nx, ny, nz, mask, soul):
+    mitama_type = soul
+    if ny:
+        for i in xrange(len(codes)):
+            if ny & (mask << (3 * i)):
+                mitama_type = codes[i]
+    return {data_format.MITAMA_COL_NAME_ZH[0]: nz,
+            attack_buf:  (nx>>24)&0xff,
+            crit_rate:   (nx>>16)&0xff,
+            crit_damage: (nx>> 8)&0xff,
+            speed:       (nx>> 0)&0xff,
+            suit:        mitama_type}
+
+def filter_loc(data_dict):
+    if len(data_dict) != 6:
+        raise KeyError("combination dict source must have 6 keys")
+
+    d1, d2, d3, d4, d5, d6 = data_dict.values()
+    print('mitama nums by loc is %s %s %s %s %s %s' % (len(d1), len(d2),
+                                                       len(d3), len(d4),
+                                                       len(d5), len(d6)))
+    def prop_value_quick(mitama):
+        mitama_info = mitama.values()[0]
+        if (mitama_info[speed] and mitama_info[speed] >= speed_1p_limit):
+            return True
+        enhance_type = data_format.MITAMA_ENHANCE[mitama_info[suit]][u"加成类型"]
+        if (enhance_type != u"攻击加成" and enhance_type != u"暴击"):
+            return False
+        if (mitama_info[attack_buf] and mitama_info[attack_buf] > 0):
+            return True
+        if (mitama_info[crit_rate] and mitama_info[crit_rate] > 0):
+            return True
+        if (mitama_info[crit_damage] and mitama_info[crit_damage] > 0):
+            return True
+        return False
+    d1 = filter(prop_value_quick, d1)
+    d2 = filter(prop_value_quick, d2)
+    #for i in d2:
+    #    print(('%s' % i).decode('raw_unicode_escape'))
+    d3 = filter(prop_value_quick, d3)
+    d4 = filter(prop_value_quick, d4)
+    d5 = filter(prop_value_quick, d5)
+    d6 = filter(prop_value_quick, d6)
+    def prop_value_attack_buf(mitama):
+        mitama_info = mitama.values()[0]
+        if (mitama_info[speed] and mitama_info[speed] >= speed_1p_limit):
+            return True
+        if (mitama_info[attack_buf] and mitama_info[attack_buf] >= 55):
+            return True
+        return False
+    d2 = filter(prop_value_attack_buf, d2)
+    d4 = filter(prop_value_attack_buf, d4)
+
+    print('after filter by loc prop %s %s %s %s %s %s' % (len(d1), len(d2),
+                                                          len(d3), len(d4),
+                                                          len(d5), len(d6)))
+    return dict(zip(range(1, 7), [d1, d2, d3, d4, d5, d6]))
+
+
+mitama_codes = []
+mitama_codes_2p_suit_mask = int(0)
+mitama_codes_4p_suit_mask = int(0)
+mitama_codes_2p_attack_buf_mask = int(0)
+mitama_codes_2p_crit_rate_mask = int(0)
+mitama_codes_4p_attack_buf_mask = int(0)
+mitama_codes_4p_crit_rate_mask = int(0)
+mitama_codes_2p_fortune_mask = int(0)
+mitama_codes_4p_fortune_mask = int(0)
+mitama_codes_4p_seductress_mask = int(0)
+mitama_codes_4p_shadow_mask = int(0)
+
+def build_type_list():
+    global mitama_codes
+    global mitama_codes_2p_suit_mask
+    global mitama_codes_4p_suit_mask
+    global mitama_codes_2p_attack_buf_mask
+    global mitama_codes_4p_attack_buf_mask
+    global mitama_codes_2p_crit_rate_mask
+    global mitama_codes_4p_crit_rate_mask
+    global mitama_codes_2p_fortune_mask
+    global mitama_codes_4p_fortune_mask
+    global mitama_codes_4p_seductress_mask
+    global mitama_codes_4p_shadow_mask
+
+    for (k, v) in data_format.MITAMA_ENHANCE.items():
+        enhance_type = v[u"加成类型"]
+        if k == u'鸣屋' or k == u'破势' or k == u'针女':
+            mitama_codes_2p_suit_mask |= (2 << (3 * len(mitama_codes)))
+            mitama_codes_4p_suit_mask |= (4 << (3 * len(mitama_codes)))
+        if k == data_format.MITAMA_TYPES[4]:
+            mitama_codes_4p_seductress_mask |= (4 << (3 * len(mitama_codes)))
+        if k == data_format.MITAMA_TYPES[34]:
+            mitama_codes_4p_shadow_mask |= (4 << (3 * len(mitama_codes)))
+        if k == data_format.MITAMA_TYPES[18]:
+            mitama_codes_2p_fortune_mask |= (2 << (3 * len(mitama_codes)))
+            mitama_codes_4p_fortune_mask |= (4 << (3 * len(mitama_codes)))
+            mitama_codes.append(k)
+            continue
+        if enhance_type == u"攻击加成":
+            mitama_codes_2p_attack_buf_mask |= (2 << (3 * len(mitama_codes)))
+            mitama_codes_4p_attack_buf_mask |= (4 << (3 * len(mitama_codes)))
+            mitama_codes.append(k)
+        if enhance_type == u"暴击":
+            mitama_codes_2p_crit_rate_mask |= (2 << (3 * len(mitama_codes)))
+            mitama_codes_4p_crit_rate_mask |= (2 << (3 * len(mitama_codes)))
+            mitama_codes.append(k)
+
+def find_item(arr, k):
+    for i in arr:
+        if i.keys()[0] == k:
+            return i
+    return None
 
 def filter_fast(data_dict):
     type_seductress = u'针女'
@@ -468,6 +622,7 @@ def filter_fast(data_dict):
         global effect_min_speed
         global effect_max_speed
         global damage_min_speed
+        global damage_max_speed
         global damage_min_crit_rate
         global attack_buf_base
         global crit_damage_base
@@ -589,14 +744,63 @@ def filter_fast(data_dict):
             yield comb_data
     type_crab = u'网切'
     # seductress + crit_damage type max damage
+    def cal_seductress_understar_max_damage():
+        global effect_min_speed
+        global effect_max_speed
+        global damage_min_speed
+        global damage_max_speed
+        global damage_min_crit_rate
+        global attack_buf_base
+        global crit_damage_base
+        damage_min_speed = 0
+        damage_max_speed = 128 - 117
+        damage_min_crit_rate = 89 - 30
+        attack_buf_base = 100
+        crit_damage_base = 160
+        damage = 0
+        res = []
+        com = {}
+        desc = ''
+        for s in soul_crit:
+        #for s in [type_shadow]:
+            #print('4%s + 2%s' % (type_seductress, s))
+            def prop_value_type(mitama):
+                if mitama.keys()[0] in done:
+                    return False
+                enhance_type = mitama.values()[0][u'御魂类型']
+                return enhance_type == type_seductress or enhance_type == s
+
+            r, c, n = filter_soul(prop_value_type,
+                                  prop_value_none,
+                                  prop_value_none,
+                                  prop_value_l6_crit_damage,
+                                  build_mask_seductress_crit,
+                                  crit_rate,
+                                  False,
+                                  score_suit_buf_max_damage,
+                                  data_dict)
+            if n > damage:
+                damage = n
+                res = r
+                com = c
+        if len(res) > 0:
+            for x in res:
+                done.add(x)
+            comb_data = make_result(data_dict, res, com)
+            #print(('%d %s' % (damage, comb_data['sum'])).decode('raw_unicode_escape'))
+            print('seductress(understar+l6damage):%d,+%d' % (damage, comb_data['sum'][speed]))
+            return comb_data
+        return None
     def cal_seductress_overstar_max_damage():
         global effect_min_speed
         global effect_max_speed
         global damage_min_speed
+        global damage_max_speed
         global damage_min_crit_rate
         global attack_buf_base
         global crit_damage_base
         damage_min_speed = 12
+        damage_max_speed = 500
         damage_min_crit_rate = 89 - 30
         attack_buf_base = 100
         crit_damage_base = 160
@@ -640,11 +844,13 @@ def filter_fast(data_dict):
         global effect_min_speed
         global effect_max_speed
         global damage_min_speed
+        global damage_max_speed
         global damage_min_crit_rate
         global attack_buf_base
         global crit_damage_base
         #print('\nshadow: overstar')
         damage_min_speed = 128 - 112
+        damage_max_speed = 500
         damage_min_crit_rate = 100 - 10 - 30
         attack_buf_base = 100
         crit_damage_base = 150
@@ -1094,165 +1300,13 @@ def filter_fast(data_dict):
             #cal_fortune_max_speed,
             cal_freetype_max_speed,
             cal_seductress_overstar_max_damage,
-            cal_shadow_overstar_max_damage,
+            #cal_seductress_understar_max_damage,
+            #cal_seductress_understar_max_damage,
+            #cal_shadow_overstar_max_damage,
         ]
     for f in order:
         comb = f()
         if comb is not None:
             yield comb
     return
-
-
-def make_result(data_dict, res, com):
-    d1, d2, d3, d4, d5, d6 = data_dict.values()
-    comb_data = {'sum': com,
-                 'info': [find_item(d1, res[0]),
-                          find_item(d2, res[1]),
-                          find_item(d3, res[2]),
-                          find_item(d4, res[3]),
-                          find_item(d5, res[4]),
-                          find_item(d6, res[5])]
-                }
-    return comb_data
-
-def map2list(codes, dx):
-    l = []
-    mitama_codes_num = len(codes)
-    #print(('map2list: %s' % dx).decode('raw_unicode_escape'))
-    for i in dx:
-        code = -1
-        k = i.keys()[0]
-        v = i.values()[0]
-	#print(('map2list: %s' % v).decode('raw_unicode_escape'))
-        for j in xrange(mitama_codes_num):
-            if codes[j] == v[suit]:
-                code = j
-                break
-        val = int(0)
-        val += int(v[effect])
-        val <<= 8
-        val += (int(v[attack_buf]) + int(v[attack]*100/attack_hero))
-        val <<= 8
-        val += int(v[crit_rate])
-        val <<= 8
-        val += int(v[crit_damage])
-        val <<= 8
-        val += int(v[speed])
-        l.append([val,
-                  (1 << (3 * code)) if (code >= 0) else 0,
-                  k])
-    return l
-
-def list2map(codes, nx, ny, nz, mask, soul):
-    mitama_type = soul
-    if ny:
-        for i in xrange(len(codes)):
-            if ny & (mask << (3 * i)):
-                mitama_type = codes[i]
-    return {data_format.MITAMA_COL_NAME_ZH[0]: nz,
-            attack_buf:  (nx>>24)&0xff,
-            crit_rate:   (nx>>16)&0xff,
-            crit_damage: (nx>> 8)&0xff,
-            speed:       (nx>> 0)&0xff,
-            suit:        mitama_type}
-
-def filter_loc(data_dict):
-    if len(data_dict) != 6:
-        raise KeyError("combination dict source must have 6 keys")
-
-    d1, d2, d3, d4, d5, d6 = data_dict.values()
-    print('mitama nums by loc is %s %s %s %s %s %s' % (len(d1), len(d2),
-                                                       len(d3), len(d4),
-                                                       len(d5), len(d6)))
-    def prop_value_quick(mitama):
-        mitama_info = mitama.values()[0]
-        if (mitama_info[speed] and mitama_info[speed] >= speed_1p_limit):
-            return True
-        enhance_type = data_format.MITAMA_ENHANCE[mitama_info[suit]][u"加成类型"]
-        if (enhance_type != u"攻击加成" and enhance_type != u"暴击"):
-            return False
-        if (mitama_info[attack_buf] and mitama_info[attack_buf] > 0):
-            return True
-        if (mitama_info[crit_rate] and mitama_info[crit_rate] > 0):
-            return True
-        if (mitama_info[crit_damage] and mitama_info[crit_damage] > 0):
-            return True
-        return False
-    d1 = filter(prop_value_quick, d1)
-    d2 = filter(prop_value_quick, d2)
-    #for i in d2:
-    #    print(('%s' % i).decode('raw_unicode_escape'))
-    d3 = filter(prop_value_quick, d3)
-    d4 = filter(prop_value_quick, d4)
-    d5 = filter(prop_value_quick, d5)
-    d6 = filter(prop_value_quick, d6)
-    def prop_value_attack_buf(mitama):
-        mitama_info = mitama.values()[0]
-        if (mitama_info[speed] and mitama_info[speed] >= speed_1p_limit):
-            return True
-        if (mitama_info[attack_buf] and mitama_info[attack_buf] >= 55):
-            return True
-        return False
-    d2 = filter(prop_value_attack_buf, d2)
-    d4 = filter(prop_value_attack_buf, d4)
-
-    print('after filter by loc prop %s %s %s %s %s %s' % (len(d1), len(d2),
-                                                          len(d3), len(d4),
-                                                          len(d5), len(d6)))
-    return dict(zip(range(1, 7), [d1, d2, d3, d4, d5, d6]))
-
-
-mitama_codes = []
-mitama_codes_2p_suit_mask = int(0)
-mitama_codes_4p_suit_mask = int(0)
-mitama_codes_2p_attack_buf_mask = int(0)
-mitama_codes_2p_crit_rate_mask = int(0)
-mitama_codes_4p_attack_buf_mask = int(0)
-mitama_codes_4p_crit_rate_mask = int(0)
-mitama_codes_2p_fortune_mask = int(0)
-mitama_codes_4p_fortune_mask = int(0)
-mitama_codes_4p_seductress_mask = int(0)
-mitama_codes_4p_shadow_mask = int(0)
-
-def build_type_list():
-    global mitama_codes
-    global mitama_codes_2p_suit_mask
-    global mitama_codes_4p_suit_mask
-    global mitama_codes_2p_attack_buf_mask
-    global mitama_codes_4p_attack_buf_mask
-    global mitama_codes_2p_crit_rate_mask
-    global mitama_codes_4p_crit_rate_mask
-    global mitama_codes_2p_fortune_mask
-    global mitama_codes_4p_fortune_mask
-    global mitama_codes_4p_seductress_mask
-    global mitama_codes_4p_shadow_mask
-
-    for (k, v) in data_format.MITAMA_ENHANCE.items():
-        enhance_type = v[u"加成类型"]
-        if k == u'鸣屋' or k == u'破势' or k == u'针女':
-            mitama_codes_2p_suit_mask |= (2 << (3 * len(mitama_codes)))
-            mitama_codes_4p_suit_mask |= (4 << (3 * len(mitama_codes)))
-        if k == data_format.MITAMA_TYPES[4]:
-            mitama_codes_4p_seductress_mask |= (4 << (3 * len(mitama_codes)))
-        if k == data_format.MITAMA_TYPES[34]:
-            mitama_codes_4p_shadow_mask |= (4 << (3 * len(mitama_codes)))
-        if k == data_format.MITAMA_TYPES[18]:
-            mitama_codes_2p_fortune_mask |= (2 << (3 * len(mitama_codes)))
-            mitama_codes_4p_fortune_mask |= (4 << (3 * len(mitama_codes)))
-            mitama_codes.append(k)
-            continue
-        if enhance_type == u"攻击加成":
-            mitama_codes_2p_attack_buf_mask |= (2 << (3 * len(mitama_codes)))
-            mitama_codes_4p_attack_buf_mask |= (4 << (3 * len(mitama_codes)))
-            mitama_codes.append(k)
-        if enhance_type == u"暴击":
-            mitama_codes_2p_crit_rate_mask |= (2 << (3 * len(mitama_codes)))
-            mitama_codes_4p_crit_rate_mask |= (2 << (3 * len(mitama_codes)))
-            mitama_codes.append(k)
-
-def find_item(arr, k):
-    for i in arr:
-        if i.keys()[0] == k:
-            return i
-    return None
 
