d26c3c991f495ae2b38bf7485525a5189b9e02c2
linshier <linshier@gmail.com>
2019-10-07 15:28:07 +0800

    upate fortune speed print

--- calculator_of_Onmyoji/cal_and_filter.py
+++ calculator_of_Onmyoji/cal_and_filter.py
@@ -784,12 +784,12 @@ def filter_fast(data_dict):
             comb_type = ['#' if (type_fortune == comb_data['info'][i].values()[0][suit]) else '' for i in xrange(6)]
             comb_speed[1] = comb_speed[1] - 57
             print ('%02d[%s]%s()maxspeed:%.2f,+%.2f' % (result_num, note, __[soul_x_type], base_speed + comb_data['sum'][speed] / 100.0, speedbuf / 100.0)), \
-                    '(1)%0.2f%s' % (comb_speed[0], comb_type[0]), \
-                    '57+(2)%0.2f%s' % (comb_speed[1], comb_type[1]), \
-                    '(3)%0.2f%s' % (comb_speed[2], comb_type[2]), \
-                    '(4)%0.2f%s' % (comb_speed[3], comb_type[3]), \
-                    '(5)%0.2f%s' % (comb_speed[4], comb_type[4]), \
-                    '(6)%0.2f%s' % (comb_speed[5], comb_type[5])
+                    '\n[1]     %0.2f %s' % (comb_speed[0], comb_type[0]), \
+                    '\n[2] 57+ %0.2f %s' % (comb_speed[1], comb_type[1]), \
+                    '\n[3]     %0.2f %s' % (comb_speed[2], comb_type[2]), \
+                    '\n[4]     %0.2f %s' % (comb_speed[3], comb_type[3]), \
+                    '\n[5]     %0.2f %s' % (comb_speed[4], comb_type[4]), \
+                    '\n[6]     %0.2f %s' % (comb_speed[5], comb_type[5])
             #print('%02d[%s]%s()maxspeed:%.2f,+%.2f' % (result_num, note, __[soul_x_type], n / 100.0, base_speed + comb_data['sum'][speed] / 100.0))
             return comb_data
         return None
