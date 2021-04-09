5bf12f65d177ecd7a99078043c990cb317c70afa
linshier <linshier@gmail.com>
2021-04-09 19:20:51 +0800

    add script for dashen

---
+++ analdashen.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#https://cbg-yys.res.netease.com/game_res/hero/330/330.png
+function jq_all()
+{
+    jq -r '.result.extra.bl' 
+}
+function jq_name()
+{
+    jq -r '.result.extra.bl|map(.|[.battle_result,.role_name,.d_role_name,(.battle_list|map(.|.shishen_id)),(.d_battle_list|map(.|.shishen_id))])' 
+}
+function jq_time()
+{
+    jq -r '.result.extra.bl|map(.|[.battle_result,.role_name,.d_role_name,.total_battle_time,.score,(.battle_list|map(.|.shishen_id)),(.d_battle_list|map(.|.shishen_id))])' 
+}
+function jq_simple()
+{
+    jq -r '.result.extra.bl|map(.|[.battle_result,(.battle_list|map(.|.shishen_id)),(.d_battle_list|map(.|.shishen_id))])' 
+}
+function output_role_id()
+{
+    sed -e "s;^\(\[[01]\);\1,${role_id/&/\&};"
+}
+function output_simple()
+{
+    grep .
+}
+function format()
+{
+    sed 's/ //g' \
+        |awk '{if($0~"\\],"){print $0}else{printf $0}}' \
+        |awk '{if($0~"\\]\\],"){print $0}else{printf $0}}' \
+        |grep -v '\[\]' \
+        |sed 's/\[\[/\[\
+        \[/;s/\]\]\]$/\]\]\
+        \]/' 
+}
+function format_none()
+{
+    grep .
+}
+function main()
+{
+    awk '{print $1}' \
+    |while read role_id; do
+        date="$(date +%s)"
+        curl -s 'https://bdapi.gameyw.netease.com/ky59/v1/g37_charts/oneuid?'${role_id}'&_='${date}'748&callback=Zepto'${date}'559' \
+        --compressed \
+        |grep 'Zepto'|sed 's/^Zepto.*(//;s/)$//'\
+        |jq_time \
+        |format_none \
+        |output_simple
+    done
+}
+cat xxx|main|tee test.txt
+exit 0
--- listdashen.sh
+++ listdashen.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
+#https://ds.163.com/2019/yys/dj-rank/
 
 date="$(date +%s)"
 server=all
 server=10000
+function jq_all()
+{
+    jq -r '.result|(.[])'
+}
+function jq_simple()
+{
+    #jq -r '.result'
+    jq -r '.result|map(.|[.role_id,(.small_extra.count_win*100/.small_extra.count_all|floor),.small_extra.count_all,.small_extra.role_name,.rank,((500+(.score-18000)/30)|floor)])' \
+        |sed 's/ //g' \
+        |awk '{if($0~"\\],"){print $0}else{printf $0}}' \
+        |sed 's/^\[\["//;s/\["//;s/\]\]//;s/\],//;s/",/ /;s/,/ /;' \
+
+}
+function output_simple()
+{
+    sed "s/^/server=${server}\&roleid=/"
+}
+function output_all()
+{
+    grep .
+}
+function one_server()
+{
+    server=$1
+    numpage=$2
+    for ((pn=1;pn<=${numpage};pn++))
+    do
+        curl 'https://bdapi.gameyw.netease.com/ky59/v1/g37_charts/topuids?server='${server}'&page='${pn}'&_='${date}'057&callback=Zepto'${date}'173' \
+            --compressed 2>/dev/null \
+            |sed 's/^Zepto[0-9]*(//;s/)$//' \
+            |jq_simple \
+            |output_simple
+    done
+}
+one_server 10009 10
+exit 0
+one_server "all" 10
+exit 0
 for ((server=10001;server<=15033;server++))
 do
+    if [ ${server} -ne 10009 -a 1 -eq 0 ]; then
+        continue
+    fi
     if [ ${server} -eq 10031 ]; then
         server=15001
     fi
-    for ((pn=1;pn<=1;pn++))
-    do
-        curl 'https://bdapi.gameyw.netease.com/ky59/v1/g37_charts/topuids?server='${server}'&page='${pn}'&_='${date}'057&callback=Zepto'${date}'235' \
-            --compressed 2>/dev/null \
-            |sed 's/^Zepto[0-9]*(//;s/)$//'|jq -r '.result|.[].role_id' \
-            |sed "s/^/server=${server}\&roleid=/"
-    done
+    one_server ${server} 1
 done
