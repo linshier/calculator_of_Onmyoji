231a4bf37329052674f7ec8024bdd476080630ff
linshier <linshier@gmail.com>
2021-05-19 14:34:56 +0800

    update

--- downdashen.sh
+++ downdashen.sh
@@ -10,14 +10,36 @@ function one_server() {
         sleep $[RANDOM % 5 + 1]
     done < <(./listdashen.sh ${server} ${topn0}|tee dbds/${server}/list|awk '($NF>=40){print $1,$4}')
 }
-one_server 10009 10
-exit 0
 one_server all 10
-#for ((server=10001;server<=15033;server++))
-for ((server=10021;server<=15033;server++))
+for ((server=10001;server<=15031;server++))
 do
+    if [ ${server} -eq 10010 ]; then
+        server=10014
+    fi
+    if [ ${server} -eq 10015 ]; then
+        server=10016
+    fi
     if [ ${server} -eq 10031 ]; then
         server=15001
     fi
+    if [ ${server} -eq 15010 ]; then
+        server=15014
+    fi
+    if [ ${server} -eq 15015 ]; then
+        server=15023
+    fi
+    if [ ${server} -eq 15025 ]; then
+        server=15026
+    fi
+    if [ ${server} -eq 15027 ]; then
+        server=15028
+    fi
+    if [ ${server} -eq 15030 ]; then
+        server=15031
+    fi
     one_server ${server} 10
 done
+exit 0
+
+one_server 10009 10
+exit 0
