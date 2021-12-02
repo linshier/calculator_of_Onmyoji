2d57182c26d196566e26a14119cfc53491e7f43c
linshier <linshier@gmail.com>
2021-12-02 17:54:41 +0800

    update

--- downdashen.sh
+++ downdashen.sh
@@ -11,41 +11,44 @@ function one_server() {
         echo ${r} ${id}
         ./analdashen.sh ${id} dbds/${server}/${r}
         sleep $[RANDOM % 5 + 1]
-    done < <(./listdashen.sh ${server} ${topn0}|tee dbds/${server}/list|grep ${filter}|awk '($NF>=30){print $1,$4}')
+    done < <(./listdashen.sh ${server} ${topn0}|tee dbds/${server}/list|grep ${filter}|awk '($NF>=50){print $1,$4}')
 }
-one_server 10009 10 Canon
-#exit 0
-one_server all 10 .
-exit 0
-for ((server=10001;server<=15031;server++))
-do
-    if [ ${server} -eq 10010 ]; then
-        server=10014
-    fi
-    if [ ${server} -eq 10015 ]; then
-        server=10016
-    fi
-    if [ ${server} -eq 10031 ]; then
-        server=15001
-    fi
-    if [ ${server} -eq 15010 ]; then
-        server=15014
-    fi
-    if [ ${server} -eq 15015 ]; then
-        server=15023
-    fi
-    if [ ${server} -eq 15025 ]; then
-        server=15026
-    fi
-    if [ ${server} -eq 15027 ]; then
-        server=15028
-    fi
-    if [ ${server} -eq 15030 ]; then
-        server=15031
-    fi
-    one_server ${server} 10
-done
-exit 0
-
-one_server 10009 10
+function quick()
+{
+    one_server 10009 10 Canon
+    one_server all 10 .
+}
+function allserver()
+{
+    for ((server=10001;server<=15031;server++))
+    do
+        if [ ${server} -eq 10010 ]; then
+            server=10014
+        fi
+        if [ ${server} -eq 10015 ]; then
+            server=10016
+        fi
+        if [ ${server} -eq 10031 ]; then
+            server=15001
+        fi
+        if [ ${server} -eq 15010 ]; then
+            server=15014
+        fi
+        if [ ${server} -eq 15015 ]; then
+            server=15023
+        fi
+        if [ ${server} -eq 15025 ]; then
+            server=15026
+        fi
+        if [ ${server} -eq 15027 ]; then
+            server=15028
+        fi
+        if [ ${server} -eq 15030 ]; then
+            server=15031
+        fi
+        one_server ${server} 10 .
+    done
+}
+quick
+allserver
 exit 0
